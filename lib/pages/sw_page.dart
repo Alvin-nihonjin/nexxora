de3e0312b1e66a337cfcd78c25c1b8130fc52c3f
knocknock2 <qwnhann22@gmail.com>
2026-05-19 14:18:17 +0700

    shaka

--- lib/pages/sw_page.dart
+++ lib/pages/sw_page.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+import 'package:video_player_web/video_player_web.dart';
 
 class StoryPage extends StatefulWidget {
   final String name;
   final String imgUrl;
+
   const StoryPage({super.key, required this.name, required this.imgUrl});
 
   @override
   State<StoryPage> createState() => _StoryPageState();
 }
 
+late VideoPlayerController _controller;
+
 class _StoryPageState extends State<StoryPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller =
+        VideoPlayerController.networkUrl(
+            Uri.parse(
+              'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+            ),
+          )
+          ..initialize().then((_) {
+            // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+            setState(() {});
+          });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +44,12 @@ class _StoryPageState extends State<StoryPage> {
       backgroundColor: Colors.black,
       body: Stack(
         children: [
-          Positioned.fill(
-            child: Image.asset(
-              widget.imgUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
+          _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
           Positioned(
             top: 50,
             left: 20,
@@ -54,14 +81,14 @@ class _StoryPageState extends State<StoryPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-
                 IconButton(
-                  icon: Icon(Icons.chat_bubble_outline,
-                      color: Colors.white, size: 28),
+                  icon: Icon(
+                    Icons.chat_bubble_outline,
+                    color: Colors.white,
+                    size: 28,
+                  ),
                   onPressed: () {},
                 ),
-
-
               ],
             ),
           ),
